e808a1241989c49a417357163fbd40f0a835b5e6
Umair Shakeel <91723589+screamEagles@users.noreply.github.com>
2025-02-19 12:06:25 +0500

    Add files via upload

---
+++ client/src/context/GlobalState.js
@@ -0,0 +1,93 @@
+import React, {createContext, useContext, useReducer} from "react";
+import AppReducer from "./AppReducer";
+import axios from "axios";
+
+const initialState = {
+    transactions: [
+        // { id: 1, text: 'Flower', amount: -20 },
+        // { id: 2, text: 'Salary', amount: 300 },
+        // { id: 3, text: 'Book', amount: -10 },
+        // { id: 4, text: 'Camera', amount: 150 }
+    ],
+    error: null,
+    loading: true
+}
+
+// create context
+export const GlobalContext = createContext(initialState);
+
+// provider component
+export const GlobalProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    // actions
+    async function getTransactions() {
+        try {
+            const res = await axios.get("http://localhost:5000/api/v1/transactions");
+
+            dispatch({
+                type: "GET_TRANSACTIONS",
+                payload: res.data.data
+            });
+        } catch (err) {
+            dispatch({
+                type: "TRANSACTION_ERROR",
+                payload: err.response.data.error
+            });
+        }
+    }
+
+    async function deleteTransaction(id) {
+        try {
+            await axios.delete(`http://localhost:5000/api/v1/transactions/${id}`);
+
+            dispatch({
+                type: 'DELETE_TRANSACTION',
+                payload: id,
+            });
+        } catch (err) {
+            dispatch({
+                type: "TRANSACTION_ERROR",
+                payload: err.response.data.error
+            });
+        }
+    }
+
+    async function addTransaction(transaction) {
+        const config = {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+
+        try {
+            const res = await axios.post("http://localhost:5000/api/v1/transactions", transaction, config);
+
+            dispatch({
+                type: 'ADD_TRANSACTION',
+                payload: res.data.data
+            });
+        } catch (err) {
+            dispatch({
+                type: "TRANSACTION_ERROR",
+                payload: err.response.data.error
+            });
+        }
+
+    }
+
+    return (
+        <GlobalContext.Provider 
+            value={{ 
+                transactions: state.transactions,
+                error: state.error,
+                loading: state.loading,
+                getTransactions,
+                deleteTransaction,
+                addTransaction
+            }}
+        >
+            {children}
+        </GlobalContext.Provider>
+    );
+}
